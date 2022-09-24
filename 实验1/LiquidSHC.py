1720d6a6060bbdfc632ba96452b6c9d43205e85e
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-24 16:28:35 +0800

    Debug

--- 实验1/LiquidSHC.py
+++ 实验1/LiquidSHC.py
@@ -34,8 +34,19 @@ ln_w_fit = fit_fun1(t_w,popt1[0],popt1[1])
 popt2,pcov2=curve_fit(fit_fun2,t_b,ln_b)
 ln_b_fit = fit_fun1(t_b,popt2[0],popt2[1])
 
+# 数据计算
+S_w = popt1[0]
+S_b = popt2[0]
+m_container = 130.23
+m_w = 147.79 - m_container
+m_b = 149.06 - m_container
+c_w = 4.18
+c_container = 0.389
+c_b = ((S_w / S_b) * (m_w*c_w + m_container*c_container) - (m_container*c_container)) / m_b
+print(c_b)
+
 #绘制图形
-## 1号数据绘图区
+## 数据绘图区
 plt.figure()
 plt.axis([5,20,1.35,2.75])
 plt.scatter(t_w,ln_w,marker='o',label="纯净水原数据")
@@ -44,7 +55,14 @@ plt.scatter(t_b,ln_b,marker='*',label="盐水原数据")
 plt.plot(t_b,ln_b_fit,color='blue',label="盐水拟合结果")
 plt.xlabel("$t(min)$",fontsize=12)
 plt.ylabel("$T(^text {o}C)$",fontsize=12)
+plt.annotate("$S' = %.4f$\n$S'' = %.4f$" %(S_w,S_b), xy=(6,1.6))
 plt.title("液体比热容测定结果拟合",fontsize=18)
 plt.legend()
 plt.savefig(".\LiquidSHC.png")
+
+## 数据计算区
+plt.figure()
+plt.axis([0,100,0,100])
+plt.annotate("$S' = %.4f$\n$S'' = %.4f$\n盐水比热容为:$%.4fX 10^3 J/(kg·k)$\n" %(S_w,S_b,abs(c_b)), xy=(10,40))
+plt.title("数据处理结果")
 plt.show()
