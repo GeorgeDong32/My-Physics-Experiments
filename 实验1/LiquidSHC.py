9b95e4d8c0585cf10ebd8065efc2a04492cb65f6
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-24 11:22:50 +0800

    Finish

--- 实验1/LiquidSHC.py
+++ 实验1/LiquidSHC.py
@@ -10,3 +10,40 @@ from scipy.optimize import curve_fit
 # plt防止中文乱码
 plt.rcParams['font.family'] = ['Arial Unicode MS','Microsoft YaHei','SimHei','sans-serif']
 plt.rcParams['axes.unicode_minus'] = False
+
+# 待拟合函数定义
+def fit_fun1(B, k1, b1):
+    return k1 * B + b1
+
+def fit_fun2(B, k2, b2):
+    return k2 * B + b2
+
+#数据导入
+data_w = pd.read_csv("LiquidSHC_PWater.csv")
+t_w = data_w.iloc[:,0]
+ln_w = data_w.iloc[:,1]
+
+data_b = pd.read_csv("LiquidSHC_Brine.csv")
+t_b = data_b.iloc[:,0]
+ln_b = data_b.iloc[:,1]
+
+#数据拟合
+popt1,pcov1=curve_fit(fit_fun1,t_w,ln_w)
+ln_w_fit = fit_fun1(t_w,popt1[0],popt1[1])
+
+popt2,pcov2=curve_fit(fit_fun2,t_b,ln_b)
+ln_b_fit = fit_fun1(t_b,popt2[0],popt2[1])
+
+#绘制图形
+## 1号数据绘图区
+plt.figure()
+plt.axis([5,20,1.35,2.75])
+plt.scatter(t_w,ln_w,marker='o',label="纯净水原数据")
+plt.plot(t_w,ln_w_fit,color='red',label="纯净水拟合结果")
+plt.scatter(t_b,ln_b,marker='*',label="盐水原数据")
+plt.plot(t_b,ln_b_fit,color='blue',label="盐水拟合结果")
+plt.xlabel("$t(min)$",fontsize=12)
+plt.ylabel("$T($^text {o}$C)$",fontsize=12)
+plt.title("$液体比热容测定结果拟合$",fontsize=18)
+plt.savefig(".\LiquidSHC.png")
+plt.show()
